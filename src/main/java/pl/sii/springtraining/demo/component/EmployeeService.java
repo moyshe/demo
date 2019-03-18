f324e17c7591a533a3a4d0e472bd5cf4a9a2c68c
hlewandowski <hlewandowski@pl.sii.eu>
2019-03-18 11:21:47 +0100

    Transactions

--- src/main/java/pl/sii/springtraining/demo/component/EmployeeService.java
+++ src/main/java/pl/sii/springtraining/demo/component/EmployeeService.java
@@ -4,6 +4,7 @@ import static org.springframework.util.StringUtils.isEmpty;
 
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.sii.springtraining.demo.model.Employee;
 
 @Service
@@ -35,6 +36,7 @@ public class EmployeeService {
     return repository.save(newEmployee);
   }
 
+  @Transactional(readOnly = true)
   public Employee update(Employee updatedEmployee) {
     Employee myEmployee = repository.getOne(updatedEmployee.getId());
     myEmployee.setName(updatedEmployee.getName());
@@ -42,7 +44,9 @@ public class EmployeeService {
     return repository.save(myEmployee);
   }
 
+  @Transactional(rollbackFor = RuntimeException.class)
   public void delete(Employee employee) {
     repository.delete(employee);
+    throw new RuntimeException("Failure");
   }
 }
